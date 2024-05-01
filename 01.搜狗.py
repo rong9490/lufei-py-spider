b32a4c7dc6bcc30ac160f86523fd9bb1f928521f
@hejj <hejj@tsintergy.com>
2024-05-01 10:41:53 +0800

    chore: init

---
+++ 01.搜狗.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+#- 需求: 爬取搜狗首页
+import requests
+print("01.py")
+if __name__ == "__main__":
+    url = 'https://www.sogou.com/'
+    requests.get(url=url)
+
+
---
+++ READMD.md
@@ -0,0 +1,20 @@
+# 路飞爬虫
+
+## http
+
+- User-Agent
+- Connection
+- Content-Type
+
+## 加密
+
+- 对称
+- 非对称
+- 证书
+
+## requests模块(实战编码)
+
+- 模拟浏览器发送请求
+- 
+
+## urllib(过期/麻烦)
